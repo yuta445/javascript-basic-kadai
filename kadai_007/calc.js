d9a7bdc91afad458a44f009ce9cc51e1dd1a383a
yuta445 <takemura.y@shinmaywa.co.jp>
2026-06-24 15:59:03 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,11 @@
+let num = 15; // 1以上の正の数を代入
+
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log("3と5の倍数です");
+} else if (num % 3 === 0) {
+  console.log("3の倍数です");
+} else if (num % 5 === 0) {
+  console.log("5の倍数です");
+} else {
+  console.log(num);
+}
